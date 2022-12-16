2507c04f4356f8263402dca9d1d8979db3819c9b
pmvinh <84178763+pmvinh@users.noreply.github.com>
2022-12-16 12:09:28 +0300

    Add files via upload

---
+++ Java18_8/task_8.java
@@ -0,0 +1,30 @@
+package Java18_8;
+
+import java.util.Scanner;
+
+public class task_8 {
+    public static void getKey(){
+        String key = "";
+        try {
+            System.out.print("Enter key: ");
+            if(key == "")
+                throw new ArithmeticException("Key is empty!");
+        }catch (ArithmeticException arithmeticException)
+        {
+            System.out.println(arithmeticException);
+        }
+        printDetails( key );
+    }
+    public static void printDetails(String key) {
+        try { String message = getDetails(key);
+            System.out.println( message );
+        }catch ( Exception e){
+            throw e;
+        }
+    }
+    private static String getDetails(String key) {
+        return "data for " + key; }
+    public static void main(String[] args) {
+        getKey();
+    }// end main
+}
