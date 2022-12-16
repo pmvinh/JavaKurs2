908e58b8de049a2a33df9f9f979c0532aa95ccd5
pmvinh <84178763+pmvinh@users.noreply.github.com>
2022-12-16 18:03:26 +0300

    Add files via upload

---
+++ Java32/OrdersManager.java
@@ -0,0 +1,9 @@
+package Java32;
+
+public interface OrdersManager {
+    int itemsQuantity(String itemName);
+    int itemsQuantity(MenuItem item);
+    Order[] getOrders();
+    int ordersCostSummary();
+    int ordersQuantity();
+}
