8724d8f293ab385835382dc74efcbe8f8cffa182
pmvinh <84178763+pmvinh@users.noreply.github.com>
2022-12-16 17:43:47 +0300

    Add files via upload

---
+++ Java_30/Alcoholable.java
@@ -0,0 +1,6 @@
+package Java_30;
+
+public interface Alcoholable {
+    boolean isAlcoholicDrink();
+    double getAlcoholVol();
+}
---
+++ Java_30/OrdersManager.java
@@ -0,0 +1,9 @@
+package Java_30;
+
+public interface OrdersManager {
+    int itemsQuantity(String itemName);
+    int itemsQuantity(MenuItem item);
+    Order[] getOrders();
+    int ordersCostSummary();
+    int ordersQuantity();
+}
