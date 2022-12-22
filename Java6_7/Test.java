d5b12c8e570ad3de816dffe0eeea2d167a4c1f89
pmvinh <84178763+pmvinh@users.noreply.github.com>
2022-12-22 08:01:19 +0700

    Update Test.java

--- Java6_7/Test.java
+++ Java6_7/Test.java
@@ -3,10 +3,10 @@ package Java6_7;
 public class Test {
     public static void main(String[] args) {
         Printable[] printables = new Printable[4];
-        printables[0] = new Book(100,"Nature");
-        printables[1] = new Shop("Toys",11111);
-        printables[2] = new Book(200,"House");
-        printables[3] = new Shop("Book",33221);
+        printables[0] = new Book(200,"Push");
+        printables[1] = new Shop("Grain",22222);
+        printables[2] = new Book(300,"Castle");
+        printables[3] = new Shop("Book",12345);
         for (int i = 0; i < 4; i++)
             printables[i].print();
     }
