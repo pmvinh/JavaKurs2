ba92d6b1771ba8bee430b410bf29c4b1f20cf4ff
pmvinh <84178763+pmvinh@users.noreply.github.com>
2022-12-22 07:49:27 +0700

    Update TestNameable.java

--- Java6_3/TestNameable.java
+++ Java6_3/TestNameable.java
@@ -2,13 +2,13 @@ package Java6_3;
 
 public class TestNameable {
     public static void main(String[] args) {
-        Nameable n1 = new Car("Mercedes");
+        Nameable n1 = new Car("BWM");
         n1.getName();
 
-        n1 = new Plane("Su-57");
+        n1 = new Plane("Boeing 747");
         n1.getName();
 
-        Plane p1 = new Plane("Boeing 747");
+        Plane p1 = new Plane("Airbus A380-800");
         p1.getName();
     }
 }
