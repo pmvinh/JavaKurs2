97ab8d7dfc71e079182ceb1ef563ad64ef0ce17d
pmvinh <84178763+pmvinh@users.noreply.github.com>
2022-12-22 08:04:44 +0700

    Update Tester.java

--- Java7_7/Tester.java
+++ Java7_7/Tester.java
@@ -3,15 +3,15 @@ package Java7_7;
 public class Tester {
     public static void main(String[] args) {
         Printable[] printables = new Printable[3];
-        printables[0] = new Printable("auchan");
-        printables[1] = new Printable("pere");
-        printables[2] = new Printable("ozon\n");
+        printables[0] = new Printable("Magnit");
+        printables[1] = new Printable("Perekrestok");
+        printables[2] = new Printable("Petorechka\n");
         Magazine.printMagazines(printables);
 
         Printable[] printabless = new Printable[3];
-        printabless[0] = new Printable("nature");
-        printabless[1] = new Printable("family");
-        printabless[2] = new Printable("computer");
+        printabless[0] = new Printable("Product");
+        printabless[1] = new Printable("Family");
+        printabless[2] = new Printable("Electronic");
         Book.printBook(printabless);
     }
 }
