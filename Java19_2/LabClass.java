0a0b1ec1f04677d81fdef293716fee839d13c759
pmvinh <84178763+pmvinh@users.noreply.github.com>
2022-12-22 20:46:26 +0300

    Update LabClass.java

--- Java19_2/LabClass.java
+++ Java19_2/LabClass.java
@@ -6,9 +6,9 @@ public class LabClass {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Student[] students = new Student[4];
-        students[0] = new Student("Tuan",22,"IKBO-33-21",4.5);
+        students[0] = new Student("Hung",22,"IKBO-01-21",4.5);
         students[1] = new Student("The Vinh",22,"IKBO-30-21",4.3);
-        students[2] = new Student("Vu Vinh",20,"IKBO-04-21",4);
+        students[2] = new Student("Vu Vinh",20,"IKBO-08-21",4);
         students[3]= new Student("Bang",20,"IKBO-36-22",4.2);
         LabClassDriver labClassDriver = new LabClassDriver();
         System.out.print("Enter the name to search: ");
